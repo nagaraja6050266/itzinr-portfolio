fe60e8f1a63cd7a2fd46181a844c9b9a7b76d07e
nagaraja6050266 <nagaraja6050266@gmail.com>
2024-07-11 21:51:13 +0530

    added from chatgpt in package

--- Backend/index.js
+++ Backend/index.js
@@ -94,3 +94,8 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+module.exports = app;
+module.exports.handler = (req, res) => {
+  app(req, res);
+};
